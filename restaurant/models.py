bff13569fd07d9cfb6fb9225517b904852e3a736
RTKirkbride <rtkirkbride@gmail.com>
2023-02-23 10:21:11 -0500

    Add restaurant and review apps and migrated db's.

---
+++ restaurant/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Restaurant(models.Model):
+    """Restaurant model class"""
+
+    restaurant_name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String method"""
+        return self.restaurant
+
+    def get_absolute_url(self):
+        """Get absolute URL"""
+        return reverse("reviews_detail", kwargs={"pk": self.pk})
--- review/models.py
+++ review/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Review(models.Model):
+    """Review model class"""
+
+    # restaurants = models.ForeignKey(
+    #    "restaurant_name",
+    #    on_delete=models.CASCADE,
+    # )
+
+    # User id foreign key
+    review_title = models.CharField(max_length=200)
+    email = models.EmailField()
+    body = models.TextField(max_length=500)
+    rating = models.IntegerField(default=3)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String method"""
+        return self.reviews
+
+    def get_absolute_url(self):
+        return reverse("reviews_detail", kwargs={"pk": self.pk})
